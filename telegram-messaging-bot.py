a16240ab13c6b574a8e54fc82b6bbd9b51bb6094
Ileska <ileska@tutanota.com>
2023-03-01 16:23:36 +0200

    Switch spaces to tabs and change message handler order

--- telegram-messaging-bot.py
+++ telegram-messaging-bot.py
@@ -33,98 +33,98 @@ CHAT_ID = int(getenv("CHAT_ID"))
 sent_messages = {}
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+					level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
 
 def start(update, context):
-    """Send a message when the command /start is issued."""
+	"""Send a message when the command /start is issued."""
 
-    update.message.reply_text("""Heippa! Kirjoita jotain niin se välittyy eteenpäin. Viestit ovat täysin anonyymejä, mutta vastaanottaja voi vastata viesteihisi botin välityksellä.""")
+	update.message.reply_text("""Heippa! Kirjoita jotain niin se välittyy eteenpäin. Viestit ovat täysin anonyymejä, mutta vastaanottaja voi vastata viesteihisi botin välityksellä.""")
 
 
 def error(bot, update, error):
-    """Log Errors caused by Updates."""
+	"""Log Errors caused by Updates."""
 
-    logger.warning('Update "%s" caused error "%s"', update, error)
+	logger.warning('Update "%s" caused error "%s"', update, error)
 
 def help(update, context):
-    start(context.bot, update)
+	start(context.bot, update)
 
 def whoami(update, context):
-    """Displays chat's ID"""
+	"""Displays chat's ID"""
 
-    id = update.effective_message.chat.id
-    context.bot.send_message(id, "The ID of this chat is: {}".format(id))
+	id = update.effective_message.chat.id
+	context.bot.send_message(id, "The ID of this chat is: {}".format(id))
 
 
 def robust_send_message(bot, msg, to, reply_id):
-    """A robust method for forwarding different types of messages anonymously."""
+	"""A robust method for forwarding different types of messages anonymously."""
 
-    sent = None
+	sent = None
 
-    if msg.text:
-        sent = bot.send_message(to, msg.text, reply_to_message_id=reply_id)
-    elif msg.sticker:
-        sent = bot.send_sticker(to, msg.sticker.file_id, reply_to_message_id=reply_id)
-    elif msg.photo:
-        sent = bot.send_photo(to, msg.photo[0].file_id, msg.caption, reply_to_message_id=reply_id)
-    elif msg.video:
-        sent = bot.send_video(to, msg.video.file_id, msg.caption, reply_to_message_id=reply_id)
-    elif msg.video_note:
-        sent = bot.send_video_note(to, msg.video_note.file_id, reply_to_message_id=reply_id)
-    elif msg.document:
-        sent = bot.send_document(to, msg.document.file_id, reply_to_message_id=reply_id)
-    elif msg.voice:
-        sent = bot.send_voice(to, msg.voice.file_id, reply_to_message_id=reply_id)
-    elif msg.audio:
-        sent = bot.send_audio(to, msg.audio.file_id, reply_to_message_id=reply_id)
-    elif msg.location:
-        sent = bot.send_location(to, location=msg.location, reply_to_message_id=reply_id)
-    else:
-        bot.send_message(msg.chat.id, "This message format is not supported :(")
+	if msg.text:
+		sent = bot.send_message(to, msg.text, reply_to_message_id=reply_id)
+	elif msg.sticker:
+		sent = bot.send_sticker(to, msg.sticker.file_id, reply_to_message_id=reply_id)
+	elif msg.photo:
+		sent = bot.send_photo(to, msg.photo[0].file_id, msg.caption, reply_to_message_id=reply_id)
+	elif msg.video:
+		sent = bot.send_video(to, msg.video.file_id, msg.caption, reply_to_message_id=reply_id)
+	elif msg.video_note:
+		sent = bot.send_video_note(to, msg.video_note.file_id, reply_to_message_id=reply_id)
+	elif msg.document:
+		sent = bot.send_document(to, msg.document.file_id, reply_to_message_id=reply_id)
+	elif msg.voice:
+		sent = bot.send_voice(to, msg.voice.file_id, reply_to_message_id=reply_id)
+	elif msg.audio:
+		sent = bot.send_audio(to, msg.audio.file_id, reply_to_message_id=reply_id)
+	elif msg.location:
+		sent = bot.send_location(to, location=msg.location, reply_to_message_id=reply_id)
+	else:
+		bot.send_message(msg.chat.id, "This message format is not supported :(")
 
-    return sent
+	return sent
 
 
 def send_from_private(update, context):
-    """Forward a private message sent for the bot to the receiving chat anonymously."""
+	"""Forward a private message sent for the bot to the receiving chat anonymously."""
 
-    msg = update.effective_message
+	msg = update.effective_message
 
-    sent_message = robust_send_message(context.bot, msg, CHAT_ID, None)
-    sent_messages[sent_message.message_id] = (msg.chat.id, msg.message_id)
+	sent_message = robust_send_message(context.bot, msg, CHAT_ID, None)
+	sent_messages[sent_message.message_id] = (msg.chat.id, msg.message_id)
 
 
 def reply(update, context):
-    """Forward reply from receiving chat back to the original sender."""
+	"""Forward reply from receiving chat back to the original sender."""
 
-    id = update.effective_message.reply_to_message.message_id
-    if id in sent_messages:
-        org = sent_messages[id]
-        robust_send_message(context.bot, update.effective_message, org[0], org[1])
+	id = update.effective_message.reply_to_message.message_id
+	if id in sent_messages:
+		org = sent_messages[id]
+		robust_send_message(context.bot, update.effective_message, org[0], org[1])
 
 
 def main():
-    """Main polling loop that contains all the callback handlers"""
+	"""Main polling loop that contains all the callback handlers"""
 
-    updater = Updater(token=BOT_TOKEN)
+	updater = Updater(token=BOT_TOKEN)
 
-    dp = updater.dispatcher
+	dp = updater.dispatcher
 
-    #Add handlers to the dispatchers. This defines what the bot does when it receives messages.
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("whoami", whoami))
+	#Add handlers to the dispatchers. This defines what the bot does when it receives messages.
+	dp.add_handler(CommandHandler("start", start))
+	dp.add_handler(CommandHandler("whoami", whoami))
 
-    dp.add_handler(MessageHandler(Filters.private, send_from_private))
-    dp.add_handler(MessageHandler(Filters.reply, reply))
+	dp.add_handler(MessageHandler(Filters.reply, reply))
+	dp.add_handler(MessageHandler(Filters.private, send_from_private))
 
-    dp.add_error_handler(error)
+	dp.add_error_handler(error)
 
-    updater.start_polling()
+	updater.start_polling()
 
-    updater.idle()
+	updater.idle()
 
 if __name__ == '__main__':
-    main()
+	main()
